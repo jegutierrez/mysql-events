180bbc3465662e85d5fd0b8d6ba32c7d7c5fd327
Jesus Gutierrez <jesusgut24@gmail.com>
2016-04-15 16:03:25 -0300

    Implementacion de socket.io para emitir eventos a los clientes cuando ocurran cambios en la base de datos

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 var ZongJi = require('zongji');
+var socketio = require('socket.io')
+var express = require('express')
+var http = require('http')
 
-var zongji = new ZongJi({
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  // debug: true
-});
+var port = 3000
+var app = express()
+var server = http.createServer(app)
+var io = socketio(server)
 
-zongji.on('binlog', function(evt) {
-  changes(evt, function (err, before, after) {
-    if (!err)
-      console.log(JSON.stringify(before)+' -> '+JSON.stringify(after))
-  })
-  //evt.dump()
-});
+server.listen(port, function(){
+  console.log('Listen on port '+port)
+})
 
-var changes = function (evt, callback) {
+io.on('connection', function(socket){
+  console.log('Nueva conexion '+socket.id)
+  newMysqlEvents(socket)
+})
+
+var _changes = function (evt, callback) {
   before = []
   after = []
   if (evt.rows !== undefined) {
@@ -29,14 +31,34 @@ var changes = function (evt, callback) {
   }
   callback('error', null)
 }
+  
 
-zongji.start({
-  includeEvents: ['tablemap','writerows', 'updaterows', 'deleterows'],
-  includeSchema: {'menuweb':['mewpae']}
-});
+var newMysqlEvents = function (socket) {
+  var zongji = new ZongJi({
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
+    // debug: true
+  });
 
-process.on('SIGINT', function() {
-  console.log('Got SIGINT.');
-  zongji.stop();
-  process.exit();
-});
+  zongji.start({
+    includeEvents: ['tablemap','writerows', 'updaterows', 'deleterows'],
+    includeSchema: {'menuweb':['mewpae']}
+  });
+
+  process.on('SIGINT', function() {
+    console.log('Got SIGINT.');
+    zongji.stop();
+    process.exit();
+  });
+  zongji.on('binlog', function(evt) {
+    _changes(evt, function (err, before, after) {
+      if (!err) {
+        console.log(JSON.stringify(before)+' -> '+JSON.stringify(after))
+        socket.emit('deli')
+      }
+    })
+    //evt.dump()
+  });
+
+}
